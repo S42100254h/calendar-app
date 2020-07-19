cf84c60f9f5b561ca64d3b4ebae876550df78f09
yuki sawabe <sawabe0v0@gmail.com>
2020-07-19 09:51:58 +0900

    define firstDay & firstDayIndex

--- src/components/CalendarBoard/index.jsx
+++ src/components/CalendarBoard/index.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { GridList } from "@material-ui/core";
 import "./style.css";
 
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+dayjs.locale("ja");
+
+  
+//今月の最初の日を追加
+const firstDay = dayjs().startOf("month");
+
+// 最初の日の曜日を取得
+const firstDayIndex = firstDay.day();
+
+
 const createCalendar = () => {
   // fill(0)で初期化する
   // 1~35の連番の配列を得るため、map関数でインデックス番号を取得する
@@ -11,7 +23,6 @@ const createCalendar = () => {
 const calendar = createCalendar();
 
 const CalendarBoard = () => {
-  console.log(calendar);
   return (
     <div className="container">
       <GridList className="grid" cols={7} spacing={0} cellHeight="auto">
