ab6c3a72c33c7cdf3253fd8302ca60b3358a973d
yuki sawabe <sawabe0v0@gmail.com>
2020-07-19 10:01:56 +0900

    move day over to correct area on the calendar

--- src/components/CalendarBoard/index.jsx
+++ src/components/CalendarBoard/index.jsx
@@ -6,18 +6,17 @@ import dayjs from "dayjs";
 import "dayjs/locale/ja";
 dayjs.locale("ja");
 
-  
 //今月の最初の日を追加
 const firstDay = dayjs().startOf("month");
 
 // 最初の日の曜日を取得
 const firstDayIndex = firstDay.day();
 
-
 const createCalendar = () => {
   // fill(0)で初期化する
   // 1~35の連番の配列を得るため、map関数でインデックス番号を取得する
-  return Array(35).fill(0).map((_, i) => i);
+  // 月の最初の日の値が0になるように配列の要素をシフトさせる
+  return Array(35).fill(0).map((_, i) => i - firstDayIndex);
 };
 
 const calendar = createCalendar();
