6436dcd075c1e3186689b2748b34cf07ea219916
yuki sawabe <sawabe0v0@gmail.com>
2020-07-19 09:36:13 +0900

    add createCalendar method

--- src/components/CalendarBoard/index.jsx
+++ src/components/CalendarBoard/index.jsx
@@ -2,45 +2,16 @@ import React from 'react';
 import { GridList } from "@material-ui/core";
 import "./style.css";
 
-const calendar = [
-  "29",
-  "30",
-  "10月1日",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-  "11月1日",
-  "2"
-];
+const createCalendar = () => {
+  // fill(0)で初期化する
+  // 1~35の連番の配列を得るため、map関数でインデックス番号を取得する
+  return Array(35).fill(0).map((_, i) => i);
+};
+
+const calendar = createCalendar();
 
 const CalendarBoard = () => {
+  console.log(calendar);
   return (
     <div className="container">
       <GridList className="grid" cols={7} spacing={0} cellHeight="auto">
