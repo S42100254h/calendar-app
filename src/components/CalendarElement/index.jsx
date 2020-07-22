e4ce5238604c242d79bf29191caf7dd50badd375
yuki sawabe <sawabe0v0@gmail.com>
2020-07-22 20:18:19 +0900

    chage format of first date

--- src/components/CalendarElement/index.jsx
+++ src/components/CalendarElement/index.jsx
@@ -4,6 +4,11 @@ import { Typography } from "@material-ui/core";
 
 // eslint-disable-next-line react/prop-types
 const CalendarElement = ({ day }) => {
+
+  // 月の最初にだけ月情報をつける
+  const isFirstDay = day.date() === 1;
+  const format = isFirstDay ? "M月D日" : "D";
+
   return (
     <div className="element">
       <Typography
@@ -12,7 +17,7 @@ const CalendarElement = ({ day }) => {
         variant="caption"
         component="div"
       >
-        {day.format("D")}
+        {day.format(format)}
       </Typography>
     </div>
   );
