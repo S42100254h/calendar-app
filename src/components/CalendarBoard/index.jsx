cc404406b3975cbe3b259c6fd736f2bc7eb7a3bc
yuki sawabe <sawabe0v0@gmail.com>
2020-07-23 07:33:13 +0900

    add day of the week

--- src/components/CalendarBoard/index.jsx
+++ src/components/CalendarBoard/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import CalendarElement from "../CalendarElement";
-import { GridList } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 import { createCalendar } from "../../services/calendar";
 import "./style.css";
+
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const calendar = createCalendar();
 
@@ -10,6 +12,21 @@ const CalendarBoard = () => {
   return (
     <div className="container">
       <GridList className="grid" cols={7} spacing={0} cellHeight="auto">
+        {days.map(d => (
+          <li key={d}>
+            <Typography
+              className="days"
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+            
+            
+        ))}
         {calendar.map(c => (
           <li key={c.toISOString()}>
             <CalendarElement day={c} />
