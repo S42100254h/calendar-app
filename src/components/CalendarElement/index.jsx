cff74e2f6588810731fa5520383156d1c74a4c2d
yuki sawabe <sawabe0v0@gmail.com>
2020-07-22 20:13:03 +0900

    add Typography

--- src/components/CalendarElement/index.jsx
+++ src/components/CalendarElement/index.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style.css";
+import { Typography } from "@material-ui/core";
 
 // eslint-disable-next-line react/prop-types
-const CalendarElement = ({ children }) => {
+const CalendarElement = ({ day }) => {
   return (
     <div className="element">
-      <div className="date">{children}</div>
+      <Typography
+        className="date"
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        {day.format("D")}
+      </Typography>
     </div>
   );
 };
