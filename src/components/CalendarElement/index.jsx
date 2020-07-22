79ff02fe2c9e814a87be74a52cc4d630e6d45012
yuki sawabe <sawabe0v0@gmail.com>
2020-07-23 07:54:35 +0900

    add blue marker to today

--- src/components/CalendarElement/index.jsx
+++ src/components/CalendarElement/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import { Typography } from "@material-ui/core";
+import dayjs from "dayjs";
 
 // eslint-disable-next-line react/prop-types
 const CalendarElement = ({ day }) => {
@@ -8,6 +9,11 @@ const CalendarElement = ({ day }) => {
   // 月の最初にだけ月情報をつける
   const isFirstDay = day.date() === 1;
   const format = isFirstDay ? "M月D日" : "D";
+
+  // 当日かどうか判断する
+  const today = dayjs();
+  const compareFormat = "YYYYMMDD";
+  const isToday = day.format(compareFormat) === today.format(compareFormat); 
 
   return (
     <div className="element">
@@ -17,7 +23,9 @@ const CalendarElement = ({ day }) => {
         variant="caption"
         component="div"
       >
+      <span className={isToday ? "today" : ""}>
         {day.format(format)}
+      </span>  
       </Typography>
     </div>
   );
