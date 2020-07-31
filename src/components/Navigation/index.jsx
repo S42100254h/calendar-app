14573e1ee70f25d329411304413fdd4abccabf08
yuki sawabe <sawabe0v0@gmail.com>
2020-08-01 08:27:49 +0900

    add css by withStyles

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,27 +1,35 @@
 import React from "react";
-import { IconButton, Toolbar, Typography } from "@material-ui/core";
+import { IconButton, Toolbar, Typography, withStyles } from "@material-ui/core";
 import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIos from "@material-ui/icons/ArrowForwardIos";
 import DehazeIcon from "@material-ui/icons/Dehaze";
 import DateRangeTwoToneIcon from '@material-ui/icons/DateRangeTwoTone';
 
+const StyledToobar = withStyles({
+  root: { padding: "0" }
+})(Toolbar);
+
+const StyledTypography = withStyles({
+  root: { margin: "0 30px 0 10px" }
+})(Typography);
+
 const Navigation = () => {
   return (
-    <Toolbar>
+    <StyledToobar>
       <IconButton>
         <DehazeIcon />
       </IconButton>
       <DateRangeTwoToneIcon size="large" />
-      <Typography color="textSecondary" variant="h5" component="h1">
+      <StyledTypography color="textSecondary" variant="h5" component="h1">
         カレンダー
-      </Typography>
+      </StyledTypography>
       <IconButton size="small">
         <ArrowBackIos />
       </IconButton>
       <IconButton size="small">
         <ArrowForwardIos />
       </IconButton>
-    </Toolbar>
+    </StyledToobar>
   );
 };
 
