94b4211dff5e6f89adfe24d08d60d52e9c050815
yuki sawabe <sawabe0v0@gmail.com>
2020-07-31 13:48:39 +0900

    create Navigation files

---
+++ src/components/Navigation/container.jsx
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+import Navigation from "./index";
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+const mergeProps = (stateProps, dispatchProps) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Navigation);
