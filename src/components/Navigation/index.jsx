a531a86cb0e8b68330e8dfb9300d55e94a51ccfd
yuki sawabe <sawabe0v0@gmail.com>
2020-07-31 14:54:27 +0900

    add toolbar to Navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { IconButton, Toolbar, Typography } from "@material-ui/core";
+import ArrowBackIos from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIos from "@material-ui/icons/ArrowForwardIos";
+import DehazeIcon from "@material-ui/icons/Dehaze";
+import DateRangeTwoToneIcon from '@material-ui/icons/DateRangeTwoTone';
 
 const Navigation = () => {
-  return <div>navigation</div>
+  return (
+    <Toolbar>
+      <IconButton>
+        <DehazeIcon />
+      </IconButton>
+      <DateRangeTwoToneIcon size="large" />
+      <Typography color="textSecondary" variant="h5" component="h1">
+        カレンダー
+      </Typography>
+      <IconButton size="small">
+        <ArrowBackIos />
+      </IconButton>
+      <IconButton size="small">
+        <ArrowForwardIos />
+      </IconButton>
+    </Toolbar>
+  );
 };
 
 export default Navigation;
