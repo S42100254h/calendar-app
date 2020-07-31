d5e4c703d3697948c88088e226a0a7cda1f3f3ab
yuki sawabe <sawabe0v0@gmail.com>
2020-07-31 10:31:37 +0900

    add getMonth function

--- src/services/calendar.js
+++ src/services/calendar.js
@@ -23,6 +23,10 @@ export const createCalendar = () =>
       return day;
     });
 
+export const getMonth = ({ year, month }) => {
+  return dayjs(`${year}-${month}`);
+};
+
 export const isSameDay = (d1, d2) => {
   const format = "YYYYMMDD";
   return d1.format(format) === d2.format(format);
