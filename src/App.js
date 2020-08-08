9cdbaa0f4fc56d3f8eb744766f434a6610ed13bb
yuki sawabe <sawabe0v0@gmail.com>
2020-08-08 17:39:23 +0900

    display dialog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CalendarBoard from "./components/CalendarBoard/container";
 import Navigation from "./components/Navigation/container";
+import AddScheduleDialog from "./components/AddScheduleDialog";
 
 const App = () => (
   <div>
     <Navigation />
     <CalendarBoard />
+    <AddScheduleDialog />
   </div>
 );
 
