4fa029a994eb493dbc6cf91f39ac49c3a980ad21
yuki sawabe <sawabe0v0@gmail.com>
2020-08-11 20:07:37 +0900

    add onChange event to Input form

--- src/components/AddScheduleDialog/index.jsx
+++ src/components/AddScheduleDialog/index.jsx
@@ -17,35 +17,60 @@ const Title = withStyles({
   root: { marginBottom: 32, fontSize: 22 }
 })(Input);
 
-const AddScheduleDialog = ({ schedule: { isDialogOpen } , closeDialog }) => {
-  return (
-    <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
-      <DialogContent>
-        <Title autoFocus fullWidth placeholder="タイトルと日時を追加" />
-        <Grid container spacing={1} alignItems="center" justify="space-between">
-          <Grid item>
-            <LocationOnOutlined />
+const AddScheduleDialog = ({ 
+  schedule: {
+    from: { title, location, description },
+    isDialogOpen 
+    },
+    closeDialog,
+    setSchedule
+  }) => {
+    return (
+      <Dialog open={isDialogOpen} onClose={closeDialog} maxWidth="xs" fullWidth>
+        <DialogContent>
+          <Title
+            autoFocus
+            fullWidth
+            placeholder="タイトルと日時を追加"
+            value={title}
+            onChange={(e) => setSchedule({ title: e.target.value })}
+          />
+          <Grid container spacing={1} alignItems="center" justify="space-between">
+            <Grid item>
+              <LocationOnOutlined />
+            </Grid>
+            <Grid item xs={10}>
+              <TextField
+                style={spacer}
+                fullWidth
+                placeholder="場所を追加"
+                value={location}
+                onChange={(e) => setSchedule({location: e.target.value })}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={10}>
-            <TextField style={spacer} fullWidth placeholder="場所を追加" />
+          <Grid container spacing={1} alignItems="center" justify="space-between">
+            <Grid item>
+              <NotesOutlined />
+            </Grid>
+            <Grid item xs={10}>
+              <TextField
+                style={spacer}
+                fullWidth
+                placeholder="説明を追加"
+                value={description}
+                onChange={(e) => setSchedule({ description: e.target.value })}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid container spacing={1} alignItems="center" justify="space-between">
-          <Grid item>
-            <NotesOutlined />
-          </Grid>
-          <Grid item xs={10}>
-            <TextField style={spacer} fullWidth placeholder="説明を追加" />
-          </Grid>
-        </Grid>
-      </DialogContent>
-      <DialogActions>
-        <Button color="primary" variant="outlined">
-          保存
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" variant="outlined">
+            保存
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
 };
 
 export default AddScheduleDialog;
