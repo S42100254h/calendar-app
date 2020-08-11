ef9863ebbf2dd3447ddddc06aa378bf3f386ca5f
yuki sawabe <sawabe0v0@gmail.com>
2020-08-11 19:42:50 +0900

    add addScheduleSetValue to mapDispatchToProps

--- src/components/AddScheduleDialog/container.jsx
+++ src/components/AddScheduleDialog/container.jsx
@@ -1,12 +1,15 @@
 import AddScheduleDialog from "./index";
 import { connect } from "react-redux";
-import { addScheduleCloseDialog } from "../../redux/addSchedule/action";
+import { addScheduleCloseDialog, addScheduleSetValue } from "../../redux/addSchedule/action";
 
 const mapStateToProps = state => ({ schedule: state.addSchedule });
 
 const mapDispatchToProps = dispatch => ({
   closeDialog: () => {
     dispatch(addScheduleCloseDialog());
+  },
+  setSchedule: () => {
+    dispatch(addScheduleSetValue(value));
   }
 });
 
