ac612fd7f3a331d6da8dd2d512f3d2ddbd459437
yuki sawabe <sawabe0v0@gmail.com>
2020-08-15 16:01:39 +0900

    change how to set calendar

--- src/components/CalendarBoard/container.jsx
+++ src/components/CalendarBoard/container.jsx
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 import CalendarBoard from "./index";
 import { createCalendar } from "../../services/calendar";
 import { addScheduleOpenDialog, addScheduleSetValue } from "../../redux/addSchedule/action";
+import { setSchedules } from "../../services/schedule";
 
 const mapStateToProps = state => ({
   calendar: state.calendar,
@@ -15,12 +16,17 @@ const mapDispatchToProps = dispatch => ({
   }
 });
 
-const mergeProps = (stateProps, dispatchProps) => ({
-  ...stateProps,
-  ...dispatchProps,
-  month: stateProps.calendar,
-  calendar: createCalendar(stateProps.calendar)
-});
+const mergeProps = (stateProps, dispatchProps) => {
+  const { calendar: month, schedules: { items: schedules }} = stateProps;
+  const calendar = setSchedules(createCalendar(month), schedules);
+
+  return {
+    ...stateProps,
+    ...dispatchProps,
+    month,
+    calendar
+  };
+};
 
 export default connect(
   mapStateToProps,
