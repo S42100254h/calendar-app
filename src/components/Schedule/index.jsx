1dcc9473c1795bca81fec0b7ac96174a2c6adf2b
yuki sawabe <sawabe0v0@gmail.com>
2020-08-15 16:54:38 +0900

    create Schedule component

---
+++ src/components/Schedule/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Schedule = ({ schedule }) => {
+  return (
+    <div className={schedule}>
+      {schedule.title}
+    </div>
+  );
+};
+
+export default Schedule;
