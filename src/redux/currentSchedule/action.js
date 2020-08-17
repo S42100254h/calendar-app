e67c5f6b4ff91e19f3b66bce417f539172a747a6
yuki sawabe <sawabe0v0@gmail.com>
2020-08-18 06:52:36 +0900

    create CurrentScheduleDialog component

--- src/redux/currentSchedule/action.js
+++ src/redux/currentSchedule/action.js
@@ -2,7 +2,7 @@ export const CURRENT_SCHEDULE_SET_ITEM = "CURRENT_SCHEDULE_SET_ITEM";
 export const CURRENT_SCHEDULE_OPEN_DIALOG = "CURRENT_SCHEDULE_OPEN_DIALOG";
 export const CURRENT_SCHEDULE_CLOSE_DIALOG = "CURRENT_SCHEDULE_CLOSE_DIALOG";
 
-export const currentScheduleSetItem = payload ({
+export const currentScheduleSetItem = payload  => ({
   type: CURRENT_SCHEDULE_SET_ITEM,
   payload
 });
