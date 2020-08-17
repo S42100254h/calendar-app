7242ec84fe86a583bb3f5e47731aee5bb27a0df7
yuki sawabe <sawabe0v0@gmail.com>
2020-08-18 07:09:27 +0900

    enable to display CurrentScheduleDialog

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import CalendarBoard from "./components/CalendarBoard/container";
 import Navigation from "./components/Navigation/container";
 import AddScheduleDialog from "./components/AddScheduleDialog/container";
+import CurrentScheduleDialog from "./components/CurrentScheduleDialog/container";
 
 const App = () => (
   <div>
     <Navigation />
     <CalendarBoard />
     <AddScheduleDialog />
+    <CurrentScheduleDialog />
   </div>
 );
 
