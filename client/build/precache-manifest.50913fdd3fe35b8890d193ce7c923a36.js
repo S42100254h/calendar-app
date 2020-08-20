01a53b24ce5f1158f853333ba9b99725477f2f72
yuki sawabe <sawabe0v0@gmail.com>
2020-08-20 19:25:54 +0900

    change directory

---
+++ client/build/precache-manifest.50913fdd3fe35b8890d193ce7c923a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb94e3eaa6e565a9fb73cfcec23318bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d322ac765d277d649b47",
+    "url": "/static/css/main.7d63aa9b.chunk.css"
+  },
+  {
+    "revision": "a99b48686657f5b9ecdd",
+    "url": "/static/js/2.cdd9519a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cdd9519a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d322ac765d277d649b47",
+    "url": "/static/js/main.7628b13d.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
