abf1948c04fc94a973d749eb5608734c15084505
yuki sawabe <sawabe0v0@gmail.com>
2020-08-31 16:34:07 +0900

    exec rubocop -a

--- db/migrate/20200831071730_devise_token_auth_create_users.rb
+++ db/migrate/20200831071730_devise_token_auth_create_users.rb
@@ -1,18 +1,17 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    
     create_table(:users) do |t|
       ## Required
-      t.string :provider, :null => false, :default => "email"
-      t.string :uid, :null => false, :default => ""
+      t.string :provider, null: false, default: "email"
+      t.string :uid, null: false, default: ""
 
       ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default => ""
+      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-      t.boolean  :allow_password_change, :default => false
+      t.boolean  :allow_password_change, default: false
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -38,10 +37,12 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
+    # rubocop:disable all
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,       unique: true
+    # rubocop:enable all
   end
 end
