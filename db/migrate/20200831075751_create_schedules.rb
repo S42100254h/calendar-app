5b26af855cd97e26ba36cbe794f909787705f498
yuki sawabe <sawabe0v0@gmail.com>
2020-08-31 17:17:07 +0900

    create schedule table

--- db/migrate/20200831075751_create_schedules.rb
+++ db/migrate/20200831075751_create_schedules.rb
@@ -1,6 +1,11 @@
 class CreateSchedules < ActiveRecord::Migration[5.2]
   def change
     create_table :schedules do |t|
+      t.string :title
+      t.text :description
+      t.string :location
+      t.date :date
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
