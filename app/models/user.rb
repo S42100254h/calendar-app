ed3c82171b6e3c28a0b22980563a09d037d28a59
yuki sawabe <sawabe0v0@gmail.com>
2020-09-01 20:57:35 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   has_many :schedules, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 50 }
+  # devise_token_authにより、emailの形式とpasswordの長さは下記のように設定されている。
+  # email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  # password_length = 6..128
 end
