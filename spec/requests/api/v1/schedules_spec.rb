50de6c20cd1d8c9fa53e1e614c89b521e4150f4e
yuki sawabe <sawabe0v0@gmail.com>
2020-09-03 06:43:26 +0900

    create request_spec for GET /api/v1/schedules

---
+++ spec/requests/api/v1/schedules_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Schedules", type: :request do
+  describe "GET /api/v1/schedules" do
+    subject { get(api_v1_schedules_path) }
+
+    before do
+      create_list(:schedule, 2)
+    end
+
+    it "スケジュールの一覧を取得できる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 2
+      expect(res[0].keys).to eq ["id", "title", "location", "description", "date", "user"]
+      expect(response).to have_http_status(200)
+    end
+  end
+end
