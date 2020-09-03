ac4bd5636625d92bb6a40f73fa4b11c0759b5069
yuki sawabe <sawabe0v0@gmail.com>
2020-09-03 18:51:57 +0900

    add create, show, update,destroy method to schedules_controller

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,6 +1,33 @@
 class Api::V1::SchedulesController < Api::V1::ApiController
+  def create
+    schedule = current_user.schedules.create!(schedule_params)
+    render json: schedule
+  end
+
   def index
     schedules = Schedule.all
     render json: schedules
   end
+
+  def show
+    schedule = Schedule.find(params[:id])
+    render json: schedule
+  end
+
+  def update
+    schedule = Schedule.find(params[:id])
+    schedule.update!(user_params)
+    render json: schedule
+  end
+
+  def destroy
+    schedule = Schedule.find(params[:id])
+    schedule.destroy!
+    render json: schedule
+  end
+
+  private
+    def schedule_params
+      params.require(:schedule).permit(:title, :location, :description, :date)
+    end
 end
