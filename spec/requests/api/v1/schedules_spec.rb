4ec96a08cbfb9553e07a6843368160224acef531
yuki sawabe <sawabe0v0@gmail.com>
2020-09-04 19:12:11 +0900

    create request_spec for POST /api/v1/schedules

--- spec/requests/api/v1/schedules_spec.rb
+++ spec/requests/api/v1/schedules_spec.rb
@@ -44,4 +44,20 @@ RSpec.describe "Api::V1::Schedules", type: :request do
       end
     end
   end
+
+  describe "POST /api/v1/schedules" do
+    subject { post(api_v1_schedules_path, params: params)}
+
+    before do
+      allow_any_instance_of(Api::V1::ApiController).to receive(:current_user).and_return(current_user)
+    end
+
+    let(:params) {{ schedule: attributes_for(:schedule) }}
+    let(:current_user) { create(:user) }
+
+    it "current_userに紐づけられたスケジュールが作成される" do
+      expect { subject }.to change { current_user.schedules.count }.by(1)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
