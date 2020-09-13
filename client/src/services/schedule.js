00cadec542a25c3fb38c9135925e2b70e0ee439e
yuki sawabe <sawabe0v0@gmail.com>
2020-09-13 10:25:36 +0900

    modify date error

--- client/src/services/schedule.js
+++ client/src/services/schedule.js
@@ -7,7 +7,9 @@ export const setSchedules = (calendar, schedules) =>
     schedules: schedules.filter(e => isSameDay(e.date, c))
   }));
 
+// ISOString, JSON.stringifyによりタイムゾーンが変更されてしまうことによって、
+// dayが１日前に戻ってしまうエラーを修正
 export const formatSchedule = schedule => ({
   ...schedule,
-  date: dayjs(schedule.date)
+  date: dayjs(schedule.date).add(1, "day")
 });
